ea62d0bb930f328d8203a053edeb5ff31285527c
Weston <westong123456@gmail.com>
2019-02-10 23:05:10 -0600

    Information Button Attribute
    Complete Package of the Information Button attribute that is usable in unity.

---
+++ InformationButton/Attributes/InformationAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class InformationButtonAttribute : PropertyAttribute
+{
+    public string titleText = "";
+    public string informationText = "";
+    public string buttonText = "";
+
+    public InformationButtonAttribute(string buttonText, string title, string information)
+    {
+        titleText = title;
+        informationText = information;
+        this.buttonText = buttonText;
+    }
+}
---
+++ InformationButton/Drawers/InformationButtonAttributeDrawer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(InformationButtonAttribute))]
+[CanEditMultipleObjects]
+public class InformationButtonAttributeDrawer : PropertyDrawer
+{
+
+    float buttonHeight = EditorGUIUtility.singleLineHeight;
+    InformationButtonAttribute attr;
+
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        attr = (InformationButtonAttribute)base.attribute;
+        Rect buttonRect = new Rect(position.x, position.y , position.width / 2, buttonHeight);
+        if(GUI.Button(buttonRect, attr.buttonText) )
+        {
+            DisplayMessage();
+
+        }
+        Rect adjustedRect = new Rect(position.x, position.y + buttonHeight + 2, position.width, buttonHeight);
+
+        EditorGUI.PropertyField(adjustedRect, property);
+        
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label,true) + buttonHeight +2;
+    }
+
+
+    public void DisplayMessage()
+    {
+        
+
+        EditorUtility.DisplayDialog(attr.titleText + " - Information", attr.informationText, "Ok");
+        
+
+    }
+}
